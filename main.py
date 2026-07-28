d501523473af5308b98f7fdb092d99ca5937e4e5
alcitech7-oss <alcitech7@gmail.com>
2026-07-28 19:53:36 -0300

    feat: descrição do que foi feito

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from rotas import rotas
+import time
 
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+# ===== CACHE BUSTING - FORÇA RECARREGAR CSS/IMAGENS =====
+@app.context_processor
+def inject_versao():
+    return {'versao': int(time.time())}
 
 app.register_blueprint(rotas)
 
